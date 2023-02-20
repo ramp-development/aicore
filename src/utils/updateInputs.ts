438d76b25724c345fb4d50230f1e8b905eb10e9d
g4knr <85592362+g4knr@users.noreply.github.com>
2023-02-20 10:54:23 +0000

    Tidied code

--- src/utils/updateInputs.ts
+++ src/utils/updateInputs.ts
@@ -1,13 +1,9 @@
 export const updateInputs = () => {
   for (const [key, value] of Object.entries(window.aiCoreParams)) {
-    console.log(value);
-    if (value) {
-      console.log('has value');
-      const inputs = document.querySelectorAll(`input[type="hidden"][name="${key}"]`);
-      console.log(inputs);
-      Array.from(inputs).forEach((input) => {
-        input.value = value;
-      });
-    }
+    if (!value) continue;
+    const inputs = [...document.querySelectorAll(`input[type="hidden"][name="${key}"]`)];
+    inputs.forEach((input) => {
+      input.value = value;
+    });
   }
 };
